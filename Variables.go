10ea6ff7ccde3680ddff591fe383174a45282af9
Mano Sriram <mano.sriram0@gmail.com>
2021-02-27 14:36:31 +0530

    Starting with variables.

---
+++ Variables.go
@@ -0,0 +1,12 @@
+package main;
+
+import "fmt";
+
+func main() {
+
+    // := is used when there is no information about the variable.
+
+    var a int = 12;
+    fmt.Printf("%d", a);
+    fmt.Printf("%T", a); // Type of 'a'
+}
